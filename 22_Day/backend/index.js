48f98db2b3463c755f2b2462aea6d76a9053148c
Saroj Dahal <isarojdahal@gmail.com>
2025-07-15 16:07:33 +0545

    add: Day 22 dir

--- 22_Day/backend/index.js
+++ 22_Day/backend/index.js
@@ -1,5 +1,6 @@
 // require("dotenv").config();
 import "dotenv/config";
+import bcrypt from "bcrypt";
 import express from "express";
 import * as z from "zod";
 import { MongoClient } from "mongodb";
@@ -15,39 +16,109 @@ async function main() {
     console.log("Connected successfully to database.");
 
     const db = c.db(process.env.DB_NAME);
-    const collection = db.collection("quotes");
+    const collection = db.collection("user");
     // Server
     const app = express();
     app.use(cors("*"));
     app.use(express.json());
 
-    function validate(req, res, next) {
-      const quoteSchema = z.object({
-        author: z.string().min(3).max(50),
-        quote: z.string().min(3),
+    function validateRegister(req, res, next) {
+      const registerSchema = z.object({
+        name: z
+          .string()
+          .min(3, "Name should be at least of 3 letters")
+          .max(50, "Maximum name length is 50"),
+        email: z.email(),
+        password: z
+          .string()
+          .min(5, "Password should be at least of 5 letters."),
       });
+      console.log("req.body", req.body);
+
       try {
-        quoteSchema.parse({ author: req.body.author, quote: req.body.quote });
+        registerSchema.parse({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password,
+        });
         next();
       } catch (err) {
+        console.log("err", err);
         res.status(400).json({
-          message: "Invalid data. (Unable to add new quote)",
+          message: "Invalid data. Error during registration.",
           error: err.errors,
         });
       }
     }
 
-    app.post("/quote", validate, async (req, res) => {
+    function validateLogin(req, res, next) {
+      const loginSchema = z.object({
+        email: z.email(),
+        password: z
+          .string()
+          .min(5, "Password should be at least of 5 letters."),
+      });
+      console.log("req.body", req.body);
+
+      try {
+        loginSchema.parse({
+          email: req.body.email,
+          password: req.body.password,
+        });
+        next();
+      } catch (err) {
+        console.log("err", err);
+        res.status(400).json({
+          message: "Invalid data. Error during Login.",
+          error: err.errors,
+        });
+      }
+    }
+
+    app.post("/login", validateLogin, async (req, res) => {
       const body = req.body;
       try {
+        const found = await collection.findOne({ email: req.body.email });
+
+        if (!found) {
+          return res.status(401).json({
+            message: "Invalid Credentials",
+          });
+        }
+
+        if (bcrypt.compareSync(req.body.password, found.password)) {
+          return res.json({ message: "Login successful" });
+        } else {
+          return res.status(500).json({ message: "Invalid Credentials." });
+        }
+      } catch (err) {
+        res.status(500).json({ message: "Database error", error: err.message });
+      }
+    });
+
+    app.post("/register", validateRegister, async (req, res) => {
+      const body = req.body;
+      try {
+        const salt = bcrypt.genSaltSync(10);
+        const passwordHash = bcrypt.hashSync(req.body.password, salt);
+
+        const found = await collection.findOne({ email: req.body.email });
+        console.log("found", found);
+        if (found) {
+          return res.status(400).json({
+            message: "Email already exists.",
+          });
+        }
+
         const inserted = await collection.insertOne({
-          author: body.author,
-          quote: body.quote,
+          email: body.email,
+          name: body.name,
+          password: passwordHash,
         });
         if (inserted.acknowledged) {
-          return res.json({ message: "Added new quote" });
+          return res.json({ message: "Created new User." });
         } else {
-          return res.status(500).json({ message: "Failed to add quote" });
+          return res.status(500).json({ message: "Failed to register user." });
         }
       } catch (err) {
         res.status(500).json({ message: "Database error", error: err.message });
